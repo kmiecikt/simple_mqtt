843350d8091f516091f820982d718c448a1fd2d2
Tomasz Kmiecik <tomaskmi@microsoft.com>
2020-12-31 18:32:35 +0100

    Implementation of simple_mqtt that does not monitor processes

--- lib/simple_mqtt.ex
+++ lib/simple_mqtt.ex
@@ -1,4 +1,8 @@
 defmodule SimpleMqtt do
+  alias SimpleMqtt.Subscriptions
+
+  use GenServer
+
   @type package_identifier() :: 0x0001..0xFFFF | nil
   @type qos() :: 0..2
   @type topic() :: String.t()
@@ -6,25 +10,51 @@ defmodule SimpleMqtt do
   @type payload() :: binary() | nil
 
   def start_link() do
-    :ok
+    GenServer.start_link(__MODULE__, nil)
   end
 
   def init(_) do
-    :ok
+    {:ok, Subscriptions.new()}
   end
 
   @spec subscribe(pid(), [topic_filter()]) :: :ok
-  def subscribe(_pid, _topic_filters) do
-    :ok
+  def subscribe(pid, topics) do
+    GenServer.call(pid, {:subscribe, topics})
   end
 
   @spec unsubscribe(pid(), [topic_filter()] | :all) :: :ok
-  def unsubscribe(_pid, _topic_filters) do
-    :ok
+  def unsubscribe(pid, topics) do
+    GenServer.call(pid, {:unsubscribe, topics})
   end
 
   @spec publish(pid(), topic(), payload()) :: :ok | {:error, :unknown_connection}
-  def publish(_pid, _topic, _payload) do
-    :ok
+  def publish(pid, topic, payload) do
+    GenServer.cast(pid, {:publish, topic, payload})
+  end
+
+  def handle_call({:subscribe, topics}, {from, _}, subscriptions) do
+    case Subscriptions.subscribe(subscriptions, from, topics) do
+      :error -> {:reply, :error, subscriptions}
+      new_subscriptions -> {:reply, :ok, new_subscriptions}
+    end
+  end
+
+  def handle_call({:unsubscribe, topics}, {from, _}, subscriptions) do
+    case Subscriptions.unsubscribe(subscriptions, from, topics) do
+      :error -> {:reply, :error, subscriptions}
+      {:empty, new_subscriptions} -> {:reply, :ok, new_subscriptions}
+      {:not_empty, new_subscriptions} -> {:reply, :ok, new_subscriptions}
+    end
+  end
+
+  def handle_cast({:publish, topic, payload}, subscriptions) do
+    case Subscriptions.list_matched(subscriptions, topic) do
+      :error -> {:noreply, subscriptions}
+      pids ->
+        for pid <- pids do
+          send(pid, {:simple_mqtt, topic, payload})
+        end
+        {:noreply, subscriptions}
+    end
   end
 end
--- test/simple_mqtt_test.exs
+++ test/simple_mqtt_test.exs
@@ -2,7 +2,22 @@ defmodule SimpleMqttTest do
   use ExUnit.Case
   doctest SimpleMqtt
 
-  # test "greets the world" do
-  #   assert SimpleMqtt.hello() == :world
-  # end
+  test "Current process receives subscribed notifications" do
+    {:ok, pid} = SimpleMqtt.start_link()
+    :ok = SimpleMqtt.subscribe(pid, ["things/a/+"])
+    :ok = SimpleMqtt.publish(pid, "things/a/on", "True")
+    assert_receive({:simple_mqtt, "things/a/on", "True"})
+  end
+
+  test "Current process doesn't receives notifications after unsubscribing" do
+    {:ok, pid} = SimpleMqtt.start_link()
+    :ok = SimpleMqtt.subscribe(pid, ["things/a/+"])
+    :ok = SimpleMqtt.unsubscribe(pid, :all)
+    :ok = SimpleMqtt.publish(pid, "things/a/on", "True")
+    refute_receive({:simple_mqtt, "things/a/on", }, 10)
+  end
+
+  test "Simple MQTT unregisters crashed procsses" do
+    assert False == True
+  end
 end
